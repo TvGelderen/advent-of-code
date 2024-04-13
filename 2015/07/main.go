6bda8551c52491349d8465d2651980a16432d841
Timen van Gelderen <thvangelderen@gmail.com>
2024-04-13 09:45:07 +0200

    2015 day 7 part 2

--- 2015/07/main.go
+++ 2015/07/main.go
@@ -40,11 +40,49 @@ func main() {
 		}
 	}
 
+	var strengthA uint16
 	for wire, strength := range circuit {
 		if wire == "a" {
-			fmt.Printf("Part 1: %d\n", strength)
+			strengthA = strength
+			break
 		}
 	}
+
+	fmt.Printf("Part 1: %d\n", strengthA)
+
+	circuit = make(map[string]uint16)
+	circuit["b"] = strengthA
+	backlog = make([]string, 0)
+
+	file, _ = os.Open("input.txt")
+	scanner = bufio.NewScanner(file)
+	for scanner.Scan() {
+		err = processLine(scanner.Text(), circuit)
+		if err != nil {
+			backlog = append(backlog, scanner.Text())
+		}
+	}
+
+	for len(backlog) > 0 {
+		currentBacklog := backlog
+		backlog = nil
+
+		for _, line := range currentBacklog {
+			err = processLine(line, circuit)
+			if err != nil {
+				backlog = append(backlog, line)
+			}
+		}
+	}
+
+	for wire, strength := range circuit {
+		if wire == "a" {
+			strengthA = strength
+			break
+		}
+	}
+
+	fmt.Printf("Part 2: %d\n", strengthA)
 
 	if err := scanner.Err(); err != nil {
 		fmt.Printf("Error reading from file: %v\n", err)
@@ -58,6 +96,9 @@ func processLine(line string, circuit map[string]uint16) error {
 
 	switch len(lhs) {
 	case 1:
+        if _, ok := circuit[wire]; ok {
+            return nil
+        }
 		strength, err := strconv.ParseInt(lhs[0], 10, 16)
 		if err != nil {
 			if val, ok := circuit[lhs[0]]; ok {
@@ -77,18 +118,18 @@ func processLine(line string, circuit map[string]uint16) error {
 	case 3:
 		switch lhs[1] {
 		case "OR", "AND":
-            val1, ok := circuit[lhs[0]]
-            if !ok {
-                if val, err := strconv.ParseInt(lhs[0], 10, 16); err != nil {
-                    return errors.New("Input without known signal strength")
-                } else {
-                    val1 = uint16(val)
-                }
-            }
-            val2, ok := circuit[lhs[2]]
-            if !ok {
-                return errors.New("Input without known signal strength")
-            }
+			val1, ok := circuit[lhs[0]]
+			if !ok {
+				if val, err := strconv.ParseInt(lhs[0], 10, 16); err != nil {
+					return errors.New("Input without known signal strength")
+				} else {
+					val1 = uint16(val)
+				}
+			}
+			val2, ok := circuit[lhs[2]]
+			if !ok {
+				return errors.New("Input without known signal strength")
+			}
 			if lhs[1] == "OR" {
 				circuit[wire] = val1 | val2
 			} else {
