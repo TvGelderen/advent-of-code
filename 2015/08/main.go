e664e4165493e8779847c47e057e5f7c545e86b1
Timen van Gelderen <thvangelderen@gmail.com>
2024-04-14 11:51:43 +0200

    2015 day 8 part 2

--- 2015/08/main.go
+++ 2015/08/main.go
@@ -16,19 +16,30 @@ func main() {
     
     baseCount := 0
     count := 0
+    escapes := 0
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		line, err := strconv.Unquote(scanner.Text())
+        line := scanner.Text()
+		unquoted, err := strconv.Unquote(line)
         if err != nil {
             log.Fatalf("Error unquoting string: %v", err)
         }
 
-        baseCount += len(scanner.Text())
-        count += len(line)
+        baseCount += len(line)
+        count += len(unquoted)
+
+        escapes += 2
+        for _, char := range line {
+            switch char {
+            case '"', '\\':
+                escapes++
+            }
+        }
 	}
 
     fmt.Printf("Part 1: %d\n", baseCount - count)
+    fmt.Printf("Part 2: %d\n", escapes)
 
 	if err = scanner.Err(); err != nil {
 		log.Fatalf("Error reading from file: %v\n", err)
